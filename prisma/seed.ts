b43d4b342ab2bcc55399d23f72e2e0f31e1eb197
jakedwng <jakedwng@gmail.com>
2023-06-14 08:32:51 -0400

    Create migration and seed db. Add prisma module and service

---
+++ prisma/seed.ts
@@ -0,0 +1,40 @@
+import { PrismaClient } from '@prisma/client';
+
+// initialize Prisma Client
+const prisma = new PrismaClient();
+
+async function main() {
+  // create two dummy articles
+  const user1 = await prisma.user.upsert({
+    where: { email: 'test@test.com' },
+    update: {},
+    create: {
+			name: 'test',
+      email: 'test@test.com',
+      password: 'test',
+    },
+  });
+
+  const user2 = await prisma.user.upsert({
+    where: { email: "test1@test.com" },
+    update: {},
+    create: {
+			name: 'test1',
+      email: "test1@test.com",
+      password: 'test1',
+    },
+  });
+
+  console.log({ user1, user2 });
+}
+
+// execute the main function
+main()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    // close Prisma Client at the end
+    await prisma.$disconnect();
+  });
